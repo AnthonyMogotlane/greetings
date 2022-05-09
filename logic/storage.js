3bc82e931140965ae3138b413be38e70b2157d73
Anthony Mogotlane <almogotlane@gmail.com>
2022-05-09 06:03:51 +0200

    add logic to save on local stroage

---
+++ logic/storage.js
@@ -0,0 +1,28 @@
+const storage = () => {
+    //variables
+    // let theKey;
+    // let theName;
+    //set the localStorage
+    const setStorage = (key, value) => {
+        container = [];
+    //checking if the is a key of names in the storage
+    if(localStorage.getItem(`${key}`) === null) {
+        container = [];
+    } else {
+        container = JSON.parse(localStorage.getItem(`${key}`));
+    }
+    let theName = value;
+    container.push(theName)
+
+    localStorage.setItem(`${key}`, JSON.stringify(container))
+    }
+    //getting the stored name/names
+    const getStorage = () => {
+       return JSON.parse(localStorage.getItem(`${key}`));
+    }
+
+    return{
+        setStorage,
+        getStorage
+    }
+}
