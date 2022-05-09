4320fe4231880bdc1525d85c89e00c6a1dd34d8c
Anthony Mogotlane <almogotlane@gmail.com>
2022-05-09 22:07:57 +0200

    modify storage.js logic

--- logic/storage.js
+++ logic/storage.js
@@ -1,28 +1,20 @@
 const storage = () => {
-    //variables
-    //let theKey;
-    // let theName;
     //set the localStorage
-    const setStorage = (key, value) => {
-        let container;
-        //checking if the is a key of names in the storage
-        if (localStorage.getItem(`${key}`) === null) {
-            container = [];
-        } else {
-            container = JSON.parse(localStorage.getItem(`${key}`));
-        }
-        let theName = value;
-        container.push(theName)
-
-        localStorage.setItem(`${key}`, JSON.stringify(container))
+    const setData = (key, value) => {
+        //set the storage values, with relevant key and values
+        localStorage.setItem(`${key}`, JSON.stringify(value))
     }
-    //getting the stored name/names
-    const getStorage = key => {
+    //getting the stored data from localStorage depending on the key
+    const getData = key => {
         return JSON.parse(localStorage.getItem(`${key}`));
     }
 
     return {
-        setStorage,
-        getStorage
+        setData,
+        getData
     }
 }
+
+let checkData = storage();
+checkData.setData("local", ["John", "Thabo", "Lebo"]);
+
