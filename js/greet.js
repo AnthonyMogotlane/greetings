a83ec28f26bc1ee7ba0ecf8b76d5079d1467cbe3
Anthony <almogotlane@gmail.com>
2022-05-09 16:46:36 +0200

    modify greet.js

--- js/greet.js
+++ js/greet.js
@@ -12,48 +12,88 @@ const radioIsixhosa = document.querySelector("#isixhosa");
 const greetMeBtn = document.querySelector(".greet-btn");
 //reference to display message
 const msg = document.querySelector(".msg");
+//reset counter button
+const resetBtn = document.querySelector(".reset-btn");
 //instance of greetings factory function
 let greeting = greetings();
 //instance of storage factory function
-let namesGreeted = storage();
+let namesGreeted = storage();//REMOVE
+
+//LOCAL STORAGE
+//initiating the local storage for counter
+if(localStorage.getItem("count") === null) {
+    localStorage.setItem("count", counterNum.innerHTML);
+    counterNum.innerHTML = localStorage.getItem("count");
+} else {
+    counterNum.innerHTML = Number(localStorage.getItem("count"));
+    localStorage.setItem("count", counterNum.innerHTML)
+}
+//=================================================================
 
 const displayText = () => {
-    //get the language checked
+    //Checking if the name is there or not
+    let arr = [];
+    if(localStorage.getItem("names") === null) {
+        arr = [];
+    } else {
+        arr = namesGreeted.getStorage("names");
+    }
+    for(let el of arr) {
+        console.log(el);
+    }   
+    
+    //get the language checked - to be moved to factory function
     let language;
     (radioEnglish.checked) ? language = radioEnglish.value
     :(radioSepedi.checked) ? language = radioSepedi.value
     :(radioIsixhosa.checked) ? language = radioIsixhosa.value
     :alert("select a language");
-
-    //receiving the name
+    //======================================================
+    //get the input name
     greeting.setFirstName(inputField.value)
-    //get the checked language
+    //get the language checked
     greeting.setLanguage(language);
 
-    //output the greeting msg with the name, and if the is an error display error message
+    //output the greeting msg with the input name then store the name to local storage, and if the is an error display error message
     if(greeting.getFirstName() === inputField.value) {
         //greet the user with the correct name
         msg.textContent = greeting.getGreetingMsg();
-        //if name is correct store to local storage 
-        namesGreeted.setStorage("names", inputField.value);
+
+        //if name is correct store to local storage
+        namesGreeted.setStorage("names", inputField.value);//MODIFYY
+
+        //increment counter if the name is been greeted the first time
+        //counterNum.innerHTML = greeting.getCounter();
+
+        //incrementing count in the local storage
+        counterNum.innerHTML = Number(localStorage.getItem("count")) + 1;
+        localStorage.setItem("count", counterNum.innerHTML)
     } else {
-        warningMsg.innerHTML = greeting.getGreetingMsg();
-        warningMsg.style.color = "red";
-        warningMsg.style.background = "#fff";
+        warningMsg.innerHTML = `<i class="fas fa-exclamation-triangle" style="color: red"></i> ${greeting.getGreetingMsg()}`;
+        //display warning msg for 2 seconds
         warningMsg.style.display = "block";
         setTimeout(() => {
             warningMsg.style.display = "none";
         }, 2000)
     }
 
-    //increment counter
-    if(inputField.value !== "" ) counterNum.innerHTML = greeting.getCounter();
-    
+   
 
-    //checking if the is a key of names in the storage
-    
+   
 
-    //Clearing the input field
+    //Clearing the input field when greet me button is pressed
     inputField.value = "";
 }
+
+//greet me event listener
 greetMeBtn.addEventListener("click", displayText);
+
+//reset counter event listener
+resetBtn.addEventListener("click", () => {
+
+    //removing counter from local storage
+    localStorage.clear();
+
+    //reseting the counter to zero
+    counterNum.innerHTML = 0;
+})
